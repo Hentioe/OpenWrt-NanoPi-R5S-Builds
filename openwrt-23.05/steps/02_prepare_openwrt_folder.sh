bc8424d3c0fceff1b56a49372a9f396898006c0b
Hentioe <meow.i5.br@gmail.com>
2023-10-17 02:43:21 +0800

    Remove commit fix

--- openwrt-23.05/steps/02_prepare_openwrt_folder.sh
+++ openwrt-23.05/steps/02_prepare_openwrt_folder.sh
@@ -12,7 +12,6 @@ cp -R openwrt-fresh-23.05 openwrt
 
 # freeze revision to 23.05.0
 cd openwrt
-git reset --hard bd4f415efacfc03bbe5b79ae1d39c1451f5f7385
 
 echo "Current OpenWRT commit"
 git log -1
