7bae1acf865e9c32d1f5354087901cfea5d2eb66
Hentioe <meow.i5.br@gmail.com>
2023-10-26 07:41:00 +0800

    Independent banner extra message

--- openwrt-23.05/steps/03-prepare_package.sh
+++ openwrt-23.05/steps/03-prepare_package.sh
@@ -14,12 +14,13 @@ cp -r $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/package/simple-obfs package/netwo
 # update & install feeds
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
-COMMENT="based on Anael Orlinski's scripts/patches"
+COMMENT="full"
 # Time stamp with $(date +%Y.%m.%d)
 BUILD_DATE="$(date +%Y.%m.%d)"
+BANNER_EXTRA=$($ROOTDIR/tools/render_tpl $ROOTDIR/openwrt-$OPENWRT_BRANCH/banner_extra -v build_date=$BUILD_DATE)
 echo "Write build date in openwrt : $BUILD_DATE"
-echo -e '\nBuilt by Hentioe on '${BUILD_DATE} \(${COMMENT}\)'\n' >>package/base-files/files/etc/banner
+echo -e "\n$BANNER_EXTRA ($COMMENT)\n" >>package/base-files/files/etc/banner
 sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='Built by Hentioe on ${BUILD_DATE}'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='$BANNER_EXTRA'" >>package/base-files/files/etc/openwrt_release
 
 rm -rf .config
