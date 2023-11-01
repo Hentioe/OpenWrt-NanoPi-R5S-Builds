53a9788803ac1ed9730cdca54de90911dfa513c1
Hentioe <meow.i5.br@gmail.com>
2023-11-01 08:54:43 +0800

    Remove mbedtls patchs

--- openwrt-23.05/steps/02_patch_openwrt.sh
+++ openwrt-23.05/steps/02_patch_openwrt.sh
@@ -33,12 +33,6 @@ cp -R $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/target/linux/rockchip target/linu
 # r8125 driver for r5s
 cp -R $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/package/r8125 package/kernel/
 
-# enable armv8 crypto for mbedtls
-#cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/package/mbedtls/patches/100-Implements-AES-and-GCM-with-ARMv8-Crypto-Extensions.patch \
-#   package/libs/mbedtls/patches/
-rm -rf package/libs/mbedtls
-cp -R $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/package/mbedtls package/libs/
-
 # video modules
 rm -rf package/kernel/linux/modules/video.mk
 cp $ROOTDIR/openwrt-$OPENWRT_BRANCH/patches/package/kernel/linux/modules/video.mk package/kernel/linux/modules/
